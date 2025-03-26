df9c0b1c7fe19dab7172e7c3002c459937bd1d65
Nicola <gladdiorus@gmail.com>
2025-03-26 16:56:45 +0100

    esercio prattica

---
+++ asset/logica.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const Input = document.getElementById("Input");
+  const aggiungiButton = document.getElementById("aggiungiButton");
+  const Lista = document.getElementById("Lista");
+
+  aggiungiButton.addEventListener("click", () => {
+    const taskText = Input.value.trim();
+    if (taskText === " ") return;
+
+    const listItem = document.createElement("li");
+    listItem.textContent = taskText;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.classList.add("delete-btn");
+
+    listItem.appendChild(deleteButton);
+    Lista.appendChild(listItem);
+    Input.value = " ";
+
+    listItem.addEventListener("click", () => {
+      listItem.classList.toggle("completo");
+    });
+
+    deleteButton.addEventListener("click", (e) => {
+      e.stopPropagation();
+      taskList.removeChild(listItem);
+    });
+  });
+});
